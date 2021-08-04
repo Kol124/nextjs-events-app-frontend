eae660ac358b2c48b4a69a4e0160f2730ac186a8
Kol124 <65050225+Kol124@users.noreply.github.com>
2021-08-04 19:42:44 +0100

    First Commit

---
+++ components/Footer.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <section>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+        <p>
+          <Link href="/about">About This Project</Link>
+        </p>
+      </section>
+      <p>Copyright &copy; DJ Events 2021</p>
+    </footer>
+  );
+}
