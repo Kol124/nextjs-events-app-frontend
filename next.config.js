bcba76eb73f90d1ca088ae12445d916d1b847428
David Milaham <dmilaham@gmail.com>
2023-09-28 18:21:38 +0100

    implement updates to packages and nodejs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
-    target: "serverless",
     deviceSizes: [375, 480, 768, 1024, 1440, 1920],
   },
 };
